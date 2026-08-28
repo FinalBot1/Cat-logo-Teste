966dfc4a360b61c20ebc6ab9c78d048352b371f1
FinalBot <167177891+FinalBot1@users.noreply.github.com>
2026-08-28 15:14:43 -0300

    Update modelFile URL to point to Supabase storage

--- Trainings/API/Assembly_Lubr.js
+++ Trainings/API/Assembly_Lubr.js
@@ -1,5 +1,5 @@
 export const modelFile =
-    "./Files/AS3A_Lubr.glb";
+    "https://nxhautbtejudvzsrytvi.supabase.co/storage/v1/object/sign/niagara_storage/Trainings/AS3A.glb?token=eyJraWQiOiI1NTEyNjllMi04YzlhLTQ4MTAtOGI2Yi1jNWI5Mzg3YWVmNDUiLCJhbGciOiJIUzUxMiJ9.eyJ1cmwiOiJuaWFnYXJhX3N0b3JhZ2UvVHJhaW5pbmdzL0FTM0EuZ2xiIiwic2NvcGUiOiJkb3dubG9hZCIsImlhdCI6MTc4NzkzODc3MSwiZXhwIjoyNDE4NjU4NzcxfQ.diGSikOUxh8rsxRfOrmaS9abU4lNaHXyYm-2hX2P_KMPZuRl4QURxyZk8JuhwOCdrcdvU0ToGPBy7sLAakXZIQ";
 
 export const startScreen = {
 
