93fe266aacf0674a58ee46b1bad0e78e890cc6d6
FinalBot <167177891+FinalBot1@users.noreply.github.com>
2026-08-28 11:01:21 -0300

    Add files via upload

---
+++ Assets/API_Menu/Index.js
@@ -0,0 +1,58 @@
+export const catalogo = [
+
+    {
+        texto:"285 / AS3A - Válvula Passagem Reta - Entrada e Saída Iguais - HC - Classe 150",
+        imagem:"../Assets/Imagens/AS3A_Img_00.gif",
+
+        lubrificacao:"Trainings/AS3A_Lubr.html",
+        montagem:"Trainings/AS3A_M_TR.html",
+        desmontagem:"Trainings/AS3A_D_TR.html",
+
+        catalogo3D:"Assemblies/AS3A_3D.html"
+    },
+
+    {
+        texto:"286 / AS1A - Válvula Globo Angular - Entrada e Saída Iguais - HC - Classe 150",
+        imagem:"../Assets/Imagens/AS1A_Img_00.gif",
+
+        lubrificacao:"Trainings/AS3A_Lubr.html",
+        montagem:"Trainings/AS3A_M_TR.html",
+        desmontagem:"Trainings/AS3A_D_TR.html",
+
+        catalogo3D:"Assemblies/AS1A_3D.html"
+    },
+
+    {
+        texto:"287 / AS4A - Válvula Globo Angular - Entrada e Saída Iguais - HL - Classe 150",
+        imagem:"../Assets/Imagens/AS4_Img_00.gif",
+
+        lubrificacao:"Trainings/AS3A_Lubr.html",
+        montagem:"Trainings/AS3A_M_TR.html",
+        desmontagem:"Trainings/AS3A_D_TR.html",
+
+        catalogo3D:"Assemblies/AS4A_3D.html"
+    },
+
+    {
+        texto:"CH5 / 259 - Válvula Retenção Angular - Entrada e Saída Iguais - Classe 150",
+        imagem:"../Assets/Imagens/259_Img_00.gif",
+
+        lubrificacao:"Trainings/AS3A_Lubr.html",
+        montagem:"Trainings/AS3A_M_TR.html",
+        desmontagem:"Trainings/AS3A_D_TR.html",
+
+        catalogo3D:"Assemblies/CH5_3D.html"
+    },
+
+    {
+        texto:"Penta-410 / 420 / 430 - Atuador Redutor de Engrenagens",
+        imagem:"../Assets/Imagens/Penta_Img_00.gif",
+
+        lubrificacao:"Trainings/AS3A_Lubr.html",
+        montagem:"Trainings/Penta_M_TR.html",
+        desmontagem:"Trainings/Penta_D_TR.html",
+
+        catalogo3D:"Assemblies/Penta_3D.html"
+    }
+
+];
---
+++ Assets/API_Menu/Usuarios.js
@@ -0,0 +1,16 @@
+/**
+ * Usuarios.js
+ * -----------------------------------------------------------------
+ * Lista de usuários e senhas do Catálogo Niagara.
+ *
+ * Formato: "usuario": "senha"
+ *
+ * Para adicionar, remover ou trocar uma senha, basta editar
+ * as linhas abaixo e salvar o arquivo.
+ * -----------------------------------------------------------------
+ */
+
+var USUARIOS_NIAGARA = {
+    "admin":   "niagara2026",
+    "niagara": "niagara2026"
+};
